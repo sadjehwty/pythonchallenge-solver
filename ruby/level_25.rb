19757930429aee8b6935ad80b5ef7b0d9f193d07
Davide Rizzi <sadjehwty@gmail.com>
2015-09-10 12:05:28 +0200

    change audio library in level 25

--- ruby/level_25.rb
+++ ruby/level_25.rb
@@ -1,33 +1,34 @@
 # http://www.pythonchallenge.com/pc/hex/lake.html
-require 'rubygems'
 require 'net/http'
-require 'audiofile'
+require 'ruby-audio'
 require 'tempfile'
-require 'RMagick'
+require 'rmagick'
 include Magick
 list=ImageList.new
 head="BMf*\000\000\000\000\000\0006\000\000\000(\000\000\000<\000\000\000<\000\000\000\001\000\030\000\000\000\000\0000*\000\000\023\v\000\000\023\v\000\000\000\000\000\000\000\000\000\000"
 http=Net::HTTP.new "www.pythonchallenge.com",80
 (1..25).each do |n|
- req= Net::HTTP::Get.new "/pc/hex/lake#{n}.wav"
- req.basic_auth 'butter','fly'
- ret=""
- begin
-  ret=http.start{|h| h.request req}.body
- rescue
-  redo
- end
+  req= Net::HTTP::Get.new "/pc/hex/lake#{n}.wav"
+  req.basic_auth 'butter','fly'
+  ret=""
+  begin
+    ret=http.start{|h| h.request req}.body
+  rescue
+    redo
+  end
   name="lake#{n}.png"
-  Tempfile.open("temp"){|t| 
-   t.write ret
-   t.flush
-   AudioFile.open(t.path,"r"){ |a|
-     i=Image.from_blob head+a.read
-     i[0].flip!
-     i[0].page=Rectangle.new 300, 300, (n-1).modulo(5)*60, 60*((n-1)/5)
-     list<< i[0]
-   }
-  }
+  Tempfile.open("temp") do |t|
+    t.write ret.force_encoding Encoding::ASCII_8BIT
+    t.flush
+    RubyAudio::Sound.open(t.path) do |a|
+      buf=a.read(:int, t.size)
+      i=Image.from_blob head+buf.to_a.map{|v| v.chr }
+      i[0].flip!
+      i[0].page=Rectangle.new 300, 300, (n-1).modulo(5)*60, 60*((n-1)/5)
+      list<< i[0]
+    end
+  end
 end
 list.mosaic.write "25.bmp"
 puts "Open 25.bmp"
+
