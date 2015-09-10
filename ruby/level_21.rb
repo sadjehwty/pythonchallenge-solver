1a028528239acb0e5a29e7d0de4a3f3931e73606
Davide Rizzi <sadjehwty@gmail.com>
2015-09-10 14:29:21 +0200

    fix encoding

--- ruby/level_21.rb
+++ ruby/level_21.rb
@@ -7,20 +7,20 @@ ret=""
 loop do
  case out[0..1]
   when "x\x9C"
-   out=Zlib::Inflate.inflate out
+   out=Zlib::Inflate.inflate(out).force_encoding Encoding::ASCII_8BIT
    ret<< " "
   when "BZ"
    t=Tempfile.new 'tmp'
    t.write out
    t.rewind
    bz2  = RBzip2::Decompressor.new t
-   out=bz2.read
+   out=bz2.read.force_encoding Encoding::ASCII_8BIT
    t.close
    ret<< "#"
   else
    t=out[-2..-1].reverse
    if t=="BZ" or t=="x\x9C"
-    out.reverse!
+    out=out.reverse.force_encoding Encoding::ASCII_8BIT
     ret<< "\n"
    else break end
  end
