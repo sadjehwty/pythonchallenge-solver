4577e182911962eefcae4faf9abf60d14f130c88
Davide Rizzi <sadjehwty@gmail.com>
2015-09-10 15:24:03 +0200

    remove audiofile gem

--- ruby/level_19.rb
+++ ruby/level_19.rb
@@ -1,14 +1,14 @@
 # http://www.pythonchallenge.com/pc/hex/bin.html
-require 'rubygems'
 require 'base64'
-f=Base64.decode64 File.read('../resources/19.eml')
-require 'audiofile'
-a=AudioFile.new( "19.wav", "w" )
-a.rate=22050
-a.channels=1
-a.sample_format=AudioFile::UNSIGNED
-a.bits=8
-a.write f
-a.flush
-a.close
+require 'ruby-audio'
+f=Base64.decode64(File.read('../resources/19.eml')).force_encoding Encoding::ASCII_8BIT
+b=RubyAudio::Buffer.new 'int', f.size
+f.bytes.each_with_index do |c,i|
+  b[i]=c.ord
+end
+i=RubyAudio::SoundInfo.new channels: 1, samplerate: 22050, format: RubyAudio::FORMAT_RAW|RubyAudio::FORMAT_PCM_U8
+RubyAudio::Sound.open "19.wav", "w", i do |a| 
+  a.write b
+end
 puts "Open 19.wav"
+
