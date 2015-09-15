2314aded8aa723f5c6d041e4a144354d1b8ed645
Davide Rizzi <sadjehwty@gmail.com>
2015-09-15 13:18:34 +0200

    fix level 28

--- ruby/level_28.rb
+++ ruby/level_28.rb
@@ -1,6 +1,5 @@
 # http://www.pythonchallenge.com/pc/ring/bell.html
-require 'rubygems'
-require 'RMagick'
+require 'rmagick'
 include Magick
 img=Image.read('../resources/bell.png').first
 green=""
@@ -12,7 +11,8 @@ end
 i=0
 valu=""
 while i<green.length
-  v=(green[i]-green[i+1]).abs
+  puts i
+  v=(green[i].ord-green[i+1].ord).abs
   valu<< v.chr if v!=42
   i+=2
 end
