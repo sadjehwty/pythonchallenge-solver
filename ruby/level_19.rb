bf294adb86d2b646e36212c713c9f77d2ecf090f
Davide Rizzi <sadjehwty@gmail.com>
2015-09-17 17:17:15 +0200

    fix level 19

--- ruby/level_19.rb
+++ ruby/level_19.rb
@@ -4,9 +4,9 @@ require 'ruby-audio'
 f=Base64.decode64(File.read('../resources/19.eml')).force_encoding Encoding::ASCII_8BIT
 b=RubyAudio::Buffer.new 'int', f.size
 f.bytes.each_with_index do |c,i|
-  b[i]=c.ord
+  b[i]=c.ord*256**3
 end
-i=RubyAudio::SoundInfo.new channels: 1, samplerate: 22050, format: RubyAudio::FORMAT_RAW|RubyAudio::FORMAT_PCM_U8
+i=RubyAudio::SoundInfo.new channels: 1, samplerate: 22050, format: RubyAudio::FORMAT_WAV|RubyAudio::FORMAT_PCM_U8
 RubyAudio::Sound.open "19.wav", "w", i do |a| 
   a.write b
 end
